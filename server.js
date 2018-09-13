c803af5df9237c58e273cfcb4d41e8ea0328a55b
Elizabeth Fun <elizabethfun@gmail.com>
2018-09-13 11:04:25 -0400

    changed from 3000 t0 5000

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const pokemonRouter = require('./routes/pokemonRouter');
 const app = express();
 
 //setup PORT
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 
 app.use(express.static(__dirname + '/public'));
 
